0668565ab639df74a438b48cda4d9d6ab005085b
jackroehrig <jack.roe13@gmail.com>
2022-04-14 19:57:09 -0500

    styles updated and made different nav for 1375px breakpoint

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,8 @@ import Box from '@mui/material/Box';
 // import CardActions from '@mui/material/CardActions'
 // import CardContent from '@mui/material/CardContent'
 // import Button from '@mui/material/Button';
+// import TextField from '@mui/material/TextField'
+import Divider from '@mui/material/Divider';
 
 // MUI ICONS IMPORTS
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -30,14 +32,19 @@ import { keyframes } from '@mui/system'
 
 // ELEMENT IMPORT
 import Projects from './elements/Projects';
+import AboutSVG from './elements/AboutSVG';
+
+
 
 function App() {
   // let [tabVal, setTabVal] = useState(0)
 
   const [navRef, navInView] = useInView({
     threshold: 0,
-    rootMargin: '-10%'
+    rootMargin: '-50%'
   })
+
+  
 
   const topRef = useRef(null)
   const scrollToTop = () => topRef.current.scrollIntoView({ behavior: 'smooth' })
@@ -90,17 +97,17 @@ function App() {
           <Tab disableRipple label='Contact' id='upper-tab-3' aria-controls='upper-tabpanel-3' />
         </Tabs> */}
         <Box sx={{ height: '100%', weight: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <Box sx={{ backgroundImage: 'radial-gradient(#352E52, #4B4F8C)', color: '#FF8D8D', p: [2, 3], borderRadius: '30px', border: '10px rgba(65, 60, 109)', textAlign: 'center' }}>
-            <Typography variant='h1' fontWeight={400} sx={{ fontSize: [40, 50, 60, 70, 80, 90], mb: [1, 2] }}>
+          <Box sx={{ bgcolor: '#2F2E41', color: '#FF8D8D', p: [2, 3], borderRadius: '30px', border: '10px rgba(65, 60, 109)', textAlign: 'center', boxShadow: 'inset 0 0 10px 8px black' }}>
+            <Typography variant='h1' fontWeight={600} sx={{ fontSize: [40, 50, 60, 70, 80, 90], mb: [1, 2] }}>
               Jack Roehrig
             </Typography>
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 2 }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 5 }}>
               <a href='https://linkedin.com/in/jack-roehrig' target='#blank'><LinkedInIcon sx={{
                 '&:hover': {
                   cursor: 'pointer',
                   animation: `${pulsing} 0.5s alternate infinite`
                 },
-                color: '#DCFEFF',
+                color: '#FF8D8D',
                 fontSize: [30, 40, null, null, null, null]
               }} /></a>
               {/* <a href='https://linkedin.com/in/jack-roehrig' target='#blank'><EmailIcon sx={{
@@ -115,11 +122,11 @@ function App() {
                   cursor: 'pointer',
                   animation: `${pulsing} 0.5s alternate infinite`
                 },
-                color: '#DCFEFF',
+                color: '#FF8D8D',
                 fontSize: [30, 40, null, null, null, null]
               }} /></a>
             </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 4, mt: [1, 2] }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 8, mt: [1, 2] }}>
               <Box onClick={scrollToProjects} sx={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', '&:hover': {
                   '& .MuiSvgIcon-fontSizeMedium': {
@@ -129,7 +136,7 @@ function App() {
                 }
               }}>
                 <Typography sx={{ fontSize: [15, null, 20, 20, 20, 30] }} variant='button'>Projects</Typography>
-                <ArrowDownwardIcon sx={{ color: '#DCFEFF' }} />
+                <ArrowDownwardIcon sx={{ color: '#FF8D8D' }} />
               </Box>
               <Box onClick={scrollToAbout} sx={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', '&:hover': {
@@ -140,7 +147,7 @@ function App() {
                 }
               }}>
                 <Typography sx={{ fontSize: [15, null, 20, 20, 20, 30] }} variant='button'>About</Typography>
-                <ArrowDownwardIcon sx={{ color: '#DCFEFF' }} />
+                <ArrowDownwardIcon sx={{ color: '#FF8D8D' }} />
               </Box>
               <Box onClick={scrollToContact} sx={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', '&:hover': {
@@ -151,19 +158,70 @@ function App() {
                 }
               }}>
                 <Typography sx={{ fontSize: [15, null, 20, 20, 20, 30] }} variant='button'>Contact</Typography>
-                <ArrowDownwardIcon sx={{ color: '#DCFEFF' }} />
+                <ArrowDownwardIcon sx={{ color: '#FF8D8D' }} />
               </Box>
             </Box>
           </Box>
         </Box>
       </Container>
       <span ref={navRef}>
-        <Projects scrollToTop={scrollToTop} projectsRef={projectsRef} navInView={navInView} scrollToProjects={scrollToProjects} scrollToAbout={scrollToAbout} scrollToContact={scrollToContact}/>
-        <Container maxWidth='xxl' ref={aboutRef} sx={{ height: '100vh', background: 'linear-gradient(to bottom left, #55619D, #352E52)', textAlign: 'center', py: 5}}>
-        <Typography variant='h1' component='h2' sx={{ color: '#FF8D8D', textShadow: '0 2px 10px black', fontWeight: 600, fontSize: [40, 50, 60, 70, 80, 90]}}>About Me</Typography>
+        <Projects scrollToTop={scrollToTop} projectsRef={projectsRef} navInView={navInView} scrollToProjects={scrollToProjects} scrollToAbout={scrollToAbout} scrollToContact={scrollToContact} />
+        <Container maxWidth='xxl' ref={aboutRef} sx={{ minHeight: '100vh', background: 'linear-gradient(to bottom left, #55619D, #352E52)', textAlign: 'center', py: 5 }}>
+          <Typography variant='h1' gutterBottom component='h2' sx={{ color: '#FF8D8D', textShadow: '0 2px 10px black', fontWeight: 600, fontSize: [40, 50, 70, 70, 80, 90] }}>About Me</Typography>
+          <Box sx={{ display: 'flex', justifyContent: 'center', gap: 3, flexDirection: ['column-reverse', null, null, null, 'row', null], alignItems: 'center' }}>
+            <Box sx={{ textAlign: 'left' }}>
+              <Box sx={{ width: '70%', margin: 'auto', minWidth: '250px' }}>
+                <Divider sx={{ mt: 1, '&.MuiDivider-withChildren': { '&::before': { borderTop: '5px solid #2F2E41' }, '&::after': { borderTop: '5px solid #2F2E41' } } }}>
+                  <Typography variant='h6' sx={{ color: '#FF8D8D', fontSize: [20, 30, 40, 40, 35, 35] }}>Name</Typography>
+                </Divider>
+                <Typography variant='body1' sx={{ color: 'white', ml: 3, fontWeight: 'bold', fontSize: [15, 25, 35, 35, 25, 25] }}>Jack Roehrig</Typography>
+                <Divider sx={{ mt: 1, '&.MuiDivider-withChildren': { '&::before': { borderTop: '5px solid #2F2E41' }, '&::after': { borderTop: '5px solid #2F2E41' } } }}>
+                  <Typography variant='h6' sx={{ color: '#FF8D8D', fontSize: [20, 30, 40, 40, 35, 35] }}>Age</Typography>
+                </Divider>
+                <Typography variant='body1' sx={{ color: 'white', ml: 3, fontWeight: 'bold', fontSize: [15, 25, 35, 35, 25, 25] }}>20</Typography>
+                <Divider sx={{ mt: 1, '&.MuiDivider-withChildren': { '&::before': { borderTop: '5px solid #2F2E41' }, '&::after': { borderTop: '5px solid #2F2E41' } } }}>
+                  <Typography variant='h6' sx={{ color: '#FF8D8D', fontSize: [20, 30, 40, 40, 35, 35] }}>Skills</Typography>
+                </Divider>
+                {/* USE CHIPS SAME AS PROJECTS SECTION */}
+                <Typography variant='body1' sx={{ color: 'white', ml: 3, fontWeight: 'bold', fontSize: [15, 25, 35, 35, 25, 25] }}>HTML, CSS, JS, REACT, MONGODB, PGSQL, EXPRESS, NODEJS, SASS</Typography>
+                <Divider sx={{ mt: 1, '&.MuiDivider-withChildren': { '&::before': { borderTop: '5px solid #2F2E41' }, '&::after': { borderTop: '5px solid #2F2E41' } } }}>
+                  <Typography variant='h6' sx={{ color: '#FF8D8D', fontSize: [20, 30, 40, 40, 35, 35] }}>Resume/Contact</Typography>
+                </Divider>
+                <Typography variant='body1' sx={{ color: 'white', ml: 3, fontWeight: 'bold', fontSize: [15, 25, 35, 35, 25, 25] }}>RESUME | EMAIL</Typography>
+                <Divider sx={{ mt: 1, '&.MuiDivider-withChildren': { '&::before': { borderTop: '5px solid #2F2E41' }, '&::after': { borderTop: '5px solid #2F2E41' } } }}>
+                  <Typography variant='h6' sx={{ color: '#FF8D8D', fontSize: [20, 30, 40, 40, 35, 35] }}>Hobbies</Typography>
+                </Divider>
+                <Typography variant='body1' sx={{ color: 'white', ml: 3, fontWeight: 'bold', fontSize: [15, 25, 35, 35, 25, 25] }}>Video games, watching sports, playing golf, fantasy football, and coding of course :)</Typography>
+              </Box>
+            </Box>
+            <Box sx={{ width: '70%', minWidth: '250px', maxWidth: '1000px', py: 4 }}>
+              <AboutSVG />
+            </Box>
+          </Box>
         </Container>
-        <Container maxWidth='xxl' ref={contactRef} sx={{ height: '100vh', bgcolor: 'lightgray', background: 'linear-gradient(to top left, #55619D, #352E52)' }}>
-          <Typography variant='h2' fontWeight={400} >Contact</Typography>
+        <Container maxWidth='xxl' ref={contactRef} sx={{ minHeight: '100vh', bgcolor: '#2F2E41', textAlign: 'center', py: 5, display: 'flex', flexDirection: 'column' }}>
+          <Typography variant='h1' component='h2' sx={{ color: '#FF8D8D', textShadow: '0 2px 10px black', fontWeight: 600, fontSize: [40, 50, 70, 70, 80, 90], flex: 0 }}>Contact</Typography>
+          <Typography variant='subtitle2' sx={{color: '#FF8D8D', fontWeght: 400}}>My email is <span style={{color: 'white'}}>jack.roe13@gmail.com</span> if you prefer to manually email yourself</Typography>
+          <Box sx={{ width: '80%', margin: 'auto', borderRadius: '20px', py: 3, flex: 1, minWidth: '280px' }}>
+            {/* <Typography variant='h4' sx={{ color: '#FF8D8D', fontWeight: 'bold' }}>Email Form</Typography> */}
+            <form autoComplete='off' style={{ width: '75%', margin: 'auto', textAlign: 'left', display: 'flex', flexDirection: 'column', gap: 15, alignItems: 'center' }}>
+              <section style={{ flex: 0, width: '100%' }}>
+                <label htmlFor='user-email' style={{ marginLeft: '10px', color: '#FF8D8D', fontSize: '1.2rem', fontWeight: 'bold' }}>Your Email</label><br />
+                <input id='user-email' name='user-email' style={{ backgroundColor: '#2F2E41', width: 'calc(100% - 20px)', color: 'white', border: '2px solid #FF8D8D', borderRadius: '4px', height: '20px', padding: '10px', fontSize: '1rem' }} />
+              </section>
+              <section style={{ flex: 0, width: '100%' }}>
+                <label htmlFor='user-subject' style={{ marginLeft: '10px', color: '#FF8D8D', fontSize: '1.2rem', fontWeight: 'bold' }}>Your Subject</label><br />
+                <input id='user-subject' name='user-subject' style={{ backgroundColor: '#2F2E41', width: 'calc(100% - 20px)', color: 'white', border: '2px solid #FF8D8D', borderRadius: '4px', height: '20px', padding: '10px', fontSize: '1rem' }} />
+              </section>
+              <section style={{ flex: 1, width: '100%' }}>
+                <label htmlFor='user-content' style={{ marginLeft: '10px', color: '#FF8D8D', fontSize: '1.2rem', fontWeight: 'bold' }}>Message</label><br />
+                <textarea id='user-content' name='user-content' rows='10' style={{ backgroundColor: '#2F2E41', width: 'calc(100% - 20px)', color: 'white', border: '2px solid #FF8D8D', borderRadius: '4px', padding: '10px', fontSize: '1rem' }} />
+              </section>
+              <section style={{ flex: 0, display: 'flex', justifyContent: 'center' }}>
+                <button type='submit' style={{ width: '200px', height: '45px', borderRadius: '4px', backgroundColor: '#2F2E41', color: '#FF8D8D', border: '2px solid #FF8D8D', fontSize: '1rem', fontWeight: 'bold' }}>Submit</button>
+              </section>
+            </form>
+          </Box>
         </Container>
       </span>
     </>
--- src/elements/Projects.jsx
+++ src/elements/Projects.jsx
@@ -5,7 +5,7 @@ import { useInView } from 'react-intersection-observer'
 // import { keyframes } from '@mui/material'
 
 // IMPORT IMAGES
-import solitaireIMG from '../assets/in-progress-solitaire-img.PNG';
+import euchreGIF from '../assets/euchre.gif'
 import restrantIMG from '../assets/in-progress-restrant.PNG';
 import toDoIMG from '../assets/in-progress-toDo.png';
 
@@ -22,11 +22,17 @@ import Slide from '@mui/material/Slide'
 import Zoom from '@mui/material/Zoom';
 import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
+import Divider from '@mui/material/Divider';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
 import NavigationOutlinedIcon from '@mui/icons-material/NavigationOutlined';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import WorkIcon from '@mui/icons-material/Work';
 import AccountBoxRoundedIcon from '@mui/icons-material/AccountBoxRounded';
 import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
+
+// MUI USE MEDIA QUERY IMPORT
+import useMediaQuery from '@mui/material/useMediaQuery'
 
 const Projects = (props) => {
 
@@ -45,6 +51,8 @@ const Projects = (props) => {
         // rootMargin: '-10px'
     })
 
+    const matches = useMediaQuery('(max-width: 1375px)')
+
     const projectIntersections = [{
         ref: projRef1,
         inView: ref1InView
@@ -57,21 +65,21 @@ const Projects = (props) => {
     }]
 
     const images = [{
-        url: solitaireIMG,
+        url: euchreGIF,
         // text: 'Here is my solitaire project. Something I learned with this project is the advantages and disadvantages of pure HTML, CSS, and JS especially when DOM manipulation was involved.',
         techList: (
-            <ul style={{ color: '#352E52', margin: '0 auto', height: '70px', fontWeight: 'bold' }}>
+            <ul style={{ color: 'white', margin: '0 auto', height: '70px', fontWeight: 'bold' }}>
                 <li>HTML</li>
                 <li>CSS</li>
                 <li>JS</li>
             </ul>
         ),
-        title: 'Solitaire Project',
+        title: 'Euchre Game',
         learnedList: (
-            <ul style={{ color: '#352E52', margin: '0 auto', fontSize: '18px', fontWeight: 400, display: 'flex', flexDirection: 'column', gap: 7}}>
+            <ul style={{ color: 'white', margin: '0 auto', fontSize: '18px', fontWeight: 400, display: 'flex', flexDirection: 'column', gap: 7 }}>
                 <li>Advantages in control with vanilla HTML, CSS, and JS</li>
                 <li>A solid grasp on most basic actions in HTML, CSS, and JS.</li>
-                <li>A solid grasp on many advanced actions including DOM manipulation, pseudo-classes, emmet abbreviation.</li>
+                <li>A solid grasp on many advanced actions including DOM manipulation, pseudo-classes, and semantic tags.</li>
                 <li>Disadvantages of vanilla when it comes to DOM manipulation.(Makes React seem a lot more valuable)</li>
             </ul>
         )
@@ -79,14 +87,14 @@ const Projects = (props) => {
         url: toDoIMG,
         // text: 'Here is the classic React To Do. This was the first React project I did on my own with classes???use better words??? It taught me state and props with react and dataflow. It also later showed me why hooks, context, and redux can be useful when building a react app.',
         techList: (
-            <ul style={{ color: '#352E52', margin: '0 auto', height: '70px', fontWeight: 'bold' }}>
+            <ul style={{ color: 'white', margin: '0 auto', height: '70px', fontWeight: 'bold' }}>
                 <li>REACT</li>
                 <li>CSS</li>
             </ul>
         ),
-        title: 'To-Do Project',
+        title: 'To-Do App',
         learnedList: (
-            <ul style={{ color: '#352E52', margin: '0 auto', fontSize: '18px', fontWeight: 400, display: 'flex', flexDirection: 'column', gap: 7}}>
+            <ul style={{ color: 'white', margin: '0 auto', fontSize: '18px', fontWeight: 400, display: 'flex', flexDirection: 'column', gap: 7 }}>
                 <li>Basics of React and the value in it's Virtual DOM</li>
                 <li>Using class components and passing data up and down my components using state and props</li>
                 <li>The value of conditional rendering compared to vanilla JS</li>
@@ -97,7 +105,7 @@ const Projects = (props) => {
         url: restrantIMG,
         // text: 'Here is my restaurant rating website, Rest-Rant. During this project I learned to make a MERN stack website with CRUD operations on restaurants and comments for the restaurants. I maybe also learned authentication and authorization. SQL??. AND DEPLOYING WEBSITES. AND SASS',
         techList: (
-            <ul style={{ color: '#352E52', margin: '0 auto', height: '70px', fontWeight: 'bold', display: 'flex', flexDirection: 'column', flexWrap: 'wrap' }}>
+            <ul style={{ color: 'white', margin: '0 auto', height: '70px', fontWeight: 'bold', display: 'flex', flexDirection: 'column', flexWrap: 'wrap' }}>
                 <li>MONGODB</li>
                 <li>EXPRESS</li>
                 <li>REACT</li>
@@ -105,9 +113,9 @@ const Projects = (props) => {
                 <li>SASS</li>
             </ul>
         ),
-        title: 'Rest-Rant Project',
+        title: 'Restaurant Rating App',
         learnedList: (
-            <ul style={{ color: '#352E52', margin: '0 auto', fontSize: '18px', fontWeight: 400, display: 'flex', flexDirection: 'column', gap: 7}}>
+            <ul style={{ color: 'white', margin: '0 auto', fontSize: '18px', fontWeight: 400, display: 'flex', flexDirection: 'column', gap: 7 }}>
                 <li>How to make a server using Express</li>
                 <li>How to connect and manipulate and a non-relational database using MongoDB and Mongoose</li>
                 <li>Using Node.JS to utilize JS on the backend</li>
@@ -125,14 +133,15 @@ const Projects = (props) => {
     ]
 
     return (
-        <Container maxWidth='xxl' name='projects' className='element' ref={props.projectsRef} sx={{ minHeight: '100vh', background: 'linear-gradient(to top left, #55619D, #352E52)', py: 5, textAlign: 'center', position: 'relative', overflowX: 'hidden' }}>
-            <Slide in={props.navInView} direction='left' mountOnEnter unmountOnExit={false}>
+        <Container maxWidth='xxl' name='projects' className='element' ref={props.projectsRef} sx={{ minHeight: '100vh', backgroundColor: '#2F2E41', py: 5, textAlign: 'center', position: 'relative', overflowX: 'hidden' }}>
+            {matches
+            ? <Slide in={props.navInView} direction='left'>
                 <SpeedDial
                     ariaLabel='navigation speedial'
                     direction='down'
                     icon={<NavigationOutlinedIcon />}
                     sx={{ position: 'fixed', top: '1px', right: '1px', mt: 2, mr: 2 }}
-                    FabProps={{ sx: { bgcolor: '#FF8D8D', color: '#352E52', '&:hover': { bgcolor: '#FF8D8D' } }, disableRipple: true }}
+                    FabProps={{ sx: { bgcolor: '#FF8D8D', color: '#2F2E41', '&:hover': { bgcolor: '#FF8D8D' } }, disableRipple: true }}
                 >
                     {actions.map(action => {
                         return (
@@ -143,46 +152,57 @@ const Projects = (props) => {
                                 tooltipOpen
                                 tooltipPlacement='left'
                                 onClick={action.action}
-                                FabProps={{ sx: { bgcolor: '#FF8D8D', color: '#352E52' } }}
-                                sx={{ '& .MuiSpeedDialAction-staticTooltipLabel': { bgcolor: '#FF8D8D', color: '#352E52' } }}
+                                FabProps={{ sx: { bgcolor: '#FF8D8D', color: '#2F2E41', '&:hover': {bgcolor: 'white'} } }}
+                                sx={{ '& .MuiSpeedDialAction-staticTooltipLabel': { bgcolor: '#FF8D8D', color: '#2F2E41' } }}
                             />
                         )
                     })}
                 </SpeedDial>
             </Slide>
+            : <Slide in={props.navInView} direction='right'>
+                <ButtonGroup orientation='vertical' variant='text' sx={{position: 'fixed', top: 0, left: 0, mt: 2, ml: 2, bgcolor: '#FF8D8D', width: 200, border: '2px solid #2F2E41', zIndex: 10}}> 
+                    <Button onClick={props.scrollToTop} sx={{color: '#2F2E41', borderColor: '#2F2E41!important'}}>Top</Button>
+                    <Button onClick={props.scrollToProjects} sx={{color: '#2F2E41', borderColor: '#2F2E41!important'}}>Projects</Button>
+                    <Button onClick={props.scrollToAbout} sx={{color: '#2F2E41', borderColor: '#2F2E41!important'}}>About Me</Button>
+                    <Button onClick={props.scrollToContact} sx={{color: '#2F2E41', borderColor: '#2F2E41!important'}}>Contact</Button>
+                </ButtonGroup>
+            </Slide>}
             <Box>
-                <Typography variant='h1' component='h2' sx={{ color: '#FF8D8D', textShadow: '0 2px 10px black', fontWeight: 600, fontSize: [40, 50, 60, 70, 80, 90] }}>Projects</Typography>
-                <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: 4, textAlign: 'left', mt: 5, pt: 5 }}>
+                <Typography variant='h1' component='h2' sx={{ color: '#FF8D8D', textShadow: '0 2px 10px black', fontWeight: 600, fontSize: [40, 50, 70, 70, 80, 90] }}>Projects</Typography>
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: 10, textAlign: 'left', mt: 5, pt: 5 }}>
                     {images.map((image, i) => {
                         return (
                             <Zoom in={projectIntersections[i].inView} key={i}>
-                                <Card ref={projectIntersections[i].ref} sx={{ maxWidth: 400, bgcolor: '#DCFEFF', borderRadius: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                                
+                                 <Card ref={projectIntersections[i].ref} sx={{ boxShadow: '2px 3px 15px 8px', maxWidth: 650, bgcolor: '#2F2E41', borderRadius: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
                                     <CardMedia
                                         component='img'
-                                        height='200'
+                                        height='400'
                                         image={image.url}
                                         alt={`image of ${image.title}`}
                                     />
                                     <CardContent sx={{ flex: 2 }}>
-                                        <Typography gutterBottom variant='h5' sx={{ color: '#352E52', fontWeight: 600, fontSize: [25, 30, 30, null, 35, 40], mb: 2 }}>{image.title}</Typography>
-                                        <Box sx={{ml: 0}}>
-                                            <Typography variant='subtitle1' sx={{ color: '#352E52', fontSize: [15, 20, 20, 22], fontWeight: 600, display: 'inline', p: '5px', borderRadius: '10px' }}>Tech Used</Typography>
+                                        <Typography gutterBottom variant='h5' sx={{ color: '#FF8D8D', fontWeight: 600, fontSize: [25, 30, 30, null, 35, 40], mb: 2 }}>{image.title}</Typography>
+                                        <Box sx={{ ml: 0 }}>
+                                            <Typography variant='subtitle1' sx={{ color: '#FF8D8D', fontSize: [15, 20, 20, 22], fontWeight: 600, display: 'inline', p: '5px', borderRadius: '10px' }}>Tech Used</Typography>
+                                            {/* USE CHIPS WITH TECH LOGOS */}
                                             {image.techList}
-                                            <Typography variant='subtitle1' sx={{ color: '#352E52', fontSize: [15, 20, 20, 22], fontWeight: 600, display: 'inline', p: '5px', borderRadius: '10px', mt: 2}}>Things I Learned</Typography>
+                                            <Typography variant='subtitle1' sx={{ color: '#FF8D8D', fontSize: [15, 20, 20, 22], fontWeight: 600, display: 'inline', p: '5px', borderRadius: '10px', mt: 2 }}>Things I Learned</Typography>
                                             {image.learnedList}
                                         </Box>
                                     </CardContent>
-                                    <CardActions sx={{ mr: 2, mb: 2, justifyContent: 'end', gap: 1 }}>
-                                        <Button size='medium' sx={{ bgcolor: '#DCFEFF', color: '#352E52', fontWeight: 600, '&:hover': { bgcolor: '#352E52', color: '#DCFEFF' } }}>Live</Button>
-                                        <Button size='medium' sx={{ bgcolor: '#DCFEFF', color: '#352E52', fontWeight: 600, '&:hover': { bgcolor: '#352E52', color: '#DCFEFF' } }}>Repository</Button>
+                                    <CardActions sx={{py: 0, justifyContent: 'space-evenly', gap: 1, borderTop: '2px solid #FF8D8D' }}>
+                                        <Button size='medium' sx={{ flex: 1, bgcolor: '#2F2E41', py: 2.5, color: '#FF8D8D', width: [160, 160, 160, 160, 160, 160], fontWeight: 600, '&:hover': { bgcolor: '#2F2E41' }, fontSize: [20, 20, 20, 20, 20, 20] }}>Live</Button>
+                                        <Divider orientation='vertical' sx={{flex: 0, '&.MuiDivider-vertical': {border: '1px solid #FF8D8D'}}} />
+                                        <Button size='medium' sx={{ flex: 1, bgcolor: '#2F2E41', py: 2.5, color: '#FF8D8D', width: [160, 160, 160, 160, 160, 160], fontWeight: 600, '&:hover': { bgcolor: '#2F2E41' }, fontSize: [20, 20, 20, 20, 20, 20] }}>Repository</Button>
                                     </CardActions>
-                                </Card>
+                                </Card> 
                             </Zoom>
                         )
                     })}
                 </Box>
             </Box>
-        </Container>
+        </Container >
     )
 }
 
